1c203e7c6cdde93c8bbb76d7a54dc0de35b60f31
sdockray <sean.patrick.dockray@gmail.com>
2026-07-10 18:44:27 +1000

    Fix deployment issues with subdirectory

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { unified } from '@astrojs/markdown-remark';
 import { remarkWikiLinks } from './src/lib/remark-wikilinks.mjs';
 
 export default defineConfig({
+  site: 'https://sdockray.github.io',
+  base: '/machine-listening-website-2026',
   markdown: {
     processor: unified({
       remarkPlugins: [remarkWikiLinks],
